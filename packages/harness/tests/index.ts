20d04ebe5d0db6119fb1b249199b70512b0a259f
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-06-04 15:40:15 +0100

    Add test harness implementation for request mocks

--- packages/harness/tests/index.ts
+++ packages/harness/tests/index.ts
@@ -1,5 +1,50 @@
+import { request } from '@octokit/request'
+import { Webhooks } from '@octokit/webhooks'
+import { Harness } from '../src'
+
 describe('harness', () => {
-  test('it passes', () => {
-    expect(true).toBeTruthy()
+  const harness = new Harness(async () => {
+    const webhooks = new Webhooks({ secret: 'secret' })
+
+    webhooks.on('push', async () => {
+      await request('GET /repos/ploys/tests/commits')
+    })
+
+    return {
+      webhooks() {
+        return webhooks
+      },
+    }
+  })
+
+  beforeEach(harness.setup)
+  afterEach(harness.teardown)
+
+  test('receive-with-intercept-uri', async () => {
+    await harness.run(async cx => {
+      cx.expect().get('https://api.github.com/repos/ploys/tests/commits').reply(200, [])
+
+      await cx.receive('push', {})
+    })
+  })
+
+  test('receive-with-intercept-path', async () => {
+    await harness.run(async cx => {
+      cx.expect().get('/repos/ploys/tests/commits').reply(200, [])
+
+      await cx.receive('push', {})
+    })
+  })
+
+  test('receive-without-intercept', async () => {
+    try {
+      await harness.run(async cx => {
+        await cx.receive('push', {})
+      })
+    } catch {
+      return
+    }
+
+    throw new Error('Expected an error')
   })
 })
