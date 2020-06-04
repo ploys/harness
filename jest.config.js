9460ac3f072003539b7dca1a76c1e26ab0d05d25
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-06-04 20:42:10 +0100

    Dismantle package workspace

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  roots: ['<rootDir>/packages/'],
+  roots: ['<rootDir>'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
   testEnvironment: 'node',
   testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.ts$',
-  moduleFileExtensions: ['ts', 'js'],
-  moduleNameMapper: {
-    "@ploys/harness(.*)": "<rootDir>/packages/harness$1/src/index.ts"
-  },
+  moduleFileExtensions: ['ts', 'js']
 }
